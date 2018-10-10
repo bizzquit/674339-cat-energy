05310e50c2e0794ff0d37ac6243bea9b6582e3c5
bizzquit <serg.alisionok@gmail.com>
2018-10-10 17:12:43 +0500

    started adaptive layout for tablet

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,19 @@
-var menuButton = document.querySelector(".page-header__btn");
+/*var menuButton = document.querySelector(".page-header__btn");
 var menu = document.querySelector(".page-header__items");
-var openingMenu = document.querySelector(".page-header__items--open");
+var openingMenu = document.querySelector(".page-header__items--open");*/
+var width = document.body.clientWidth;
 
-if (openingMenu) {
-  menu.classList.remove("page-header__items--open");
-  menu.classList.add("page-header__items--close");
+
+if (width < 768) {
+    var menuButton = document.querySelector(".page-header__btn");
+    var menu = document.querySelector(".page-header__items");
+    var openingMenu = document.querySelector(".page-header__items--open");
+    if (openingMenu) {
+        menu.classList.remove("page-header__items--open");
+        menu.classList.add("page-header__items--close");
+    }
+    menuButton.addEventListener("click", function () {
+        menu.classList.toggle("page-header__items--open");
+        menu.classList.toggle("page-header__items--close");
+    });
 }
-menuButton.addEventListener("click", function () {
-  menu.classList.toggle("page-header__items--open");
-  menu.classList.toggle("page-header__items--close");
-});
