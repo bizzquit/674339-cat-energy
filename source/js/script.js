7e0e1c9b7a2e01e3ab1a9a4039926f1c831b9d1a
bizzquit <serg.alisionok@gmail.com>
2018-10-11 19:08:25 +0500

    Adapted index.html under the tablet

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,33 @@
-/*var menuButton = document.querySelector(".page-header__btn");
+var menuButton = document.querySelector(".page-header__btn");
 var menu = document.querySelector(".page-header__items");
-var openingMenu = document.querySelector(".page-header__items--open");*/
+var openingMenu = document.querySelector(".page-header__items--open");
 var width = document.body.clientWidth;
 
-
 if (width < 768) {
-    var menuButton = document.querySelector(".page-header__btn");
-    var menu = document.querySelector(".page-header__items");
-    var openingMenu = document.querySelector(".page-header__items--open");
     if (openingMenu) {
         menu.classList.remove("page-header__items--open");
         menu.classList.add("page-header__items--close");
     }
-    menuButton.addEventListener("click", function () {
-        menu.classList.toggle("page-header__items--open");
-        menu.classList.toggle("page-header__items--close");
-    });
 }
+
+  /*При открытом меню в мобаил - при увеличении VP и обратно меню поганило*/
+window.addEventListener("resize", function () {
+    width = document.body.clientWidth;
+    if (width < 768) {
+        if (openingMenu) {
+            menu.classList.remove("page-header__items--open");
+            menu.classList.add("page-header__items--close");
+        }
+    }
+    if (width > 768) {
+        if (openingMenu) {
+            menu.classList.remove("page-header__items--close");
+            menu.classList.add("page-header__items--open");
+        }
+    }
+});
+
+menuButton.addEventListener("click", function () {
+    menu.classList.toggle("page-header__items--open");
+    menu.classList.toggle("page-header__items--close");
+});
