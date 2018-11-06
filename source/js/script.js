b6d7dcc467e825f72d01472c2975921cbe6157d5
bizzquit <serg.alisionok@gmail.com>
2018-11-06 19:28:36 +0500

    fixed bugs, added behavior of elements from the stack

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,18 @@
 var menuButton = document.querySelector(".page-header__btn");
 var menu = document.querySelector(".page-header__items");
 var openingMenu = document.querySelector(".page-header__items--open");
+var closeMenu = document.querySelector(".page-header__items--close");
 var width = document.body.clientWidth;
 
 if (width < 768) {
     if (openingMenu) {
         menu.classList.remove("page-header__items--open");
         menu.classList.add("page-header__items--close");
+        menuButton.classList.add("page-header__btn--close");
     }
 }
 
-  /*При открытом меню в мобаил - при увеличении VP и обратно меню поганило*/
+  /*При открытом меню в мобаил - при увеличении VP (альбом/ландшафт) и обратно меню поганило*/
 window.addEventListener("resize", function () {
     width = document.body.clientWidth;
     if (width < 768) {
@@ -30,13 +32,6 @@ window.addEventListener("resize", function () {
 menuButton.addEventListener("click", function () {
     menu.classList.toggle("page-header__items--open");
     menu.classList.toggle("page-header__items--close");
-
-    if (!openingMenu) {
-      menuButton.style.backgroundImage = 'url("../img/icon-close.svg")';
-    }
-    else{
-      menuButton.style.backgroundImage = 'url("../img/icon-mobile-menu.svg")';
-    }
-
-
+    menuButton.classList.toggle("page-header__btn--close");
+    menuButton.classList.toggle("page-header__btn--open");
 });
