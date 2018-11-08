6d238618216ec1f81bb3650ab3230c7db1978e8f
bizzquit <serg.alisionok@gmail.com>
2018-11-08 19:36:28 +0500

    added js files and small corrects

--- source/js/valid.js
+++ source/js/valid.js
@@ -1,11 +1,14 @@
 var arr = document.querySelectorAll(".text-item__input--required");
 
-for (var i = 0; i < arr.length; i++) {
+for (var i = 0; i < arr.length; i++)
+{
   arr[i].onblur = function() {
     var text = this.value;
     if(!text) {
-      this.classList.add("error")
-      this.style.border = "2px solid #ff8282";
+      this.classList.add("text-item__input--error");
+    }
+    if(text) {
+      this.classList.remove("text-item__input--error");
     }
   };
 }
